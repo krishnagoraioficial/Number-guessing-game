be4d788eef6c472a39b795d4d3daa280ffe5ae80
krishnagoraioficial <krishnagoraioficial@gmail.com>
2025-04-14 14:50:00 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    // Variables
+    int guess;
+    int random;
+    int count = 1;
+    // Generating a random numbers using rand function
+    srand(time(0));
+    random = (rand() % 100) + 1;
+
+    // ####### Welcome Window ######## //
+
+    printf("\t====== Welcome to Number gussing game ======\n\n");
+
+    printf("Enter a number between 1 to 100: ");
+
+here: // Label for goto function
+    scanf("%d", &guess);
+    if (guess < random)
+    {
+        printf("Try a big number: ");
+        count++;
+        goto here;
+    }
+    else if (guess > random)
+    {
+        printf("Try small number: ");
+        count++;
+        goto here;
+    }
+    else
+    {
+        printf("\t+++ Congrats you got the number in %d try +++\n", count);
+        printf("\n\n\tThank you for using this\n\t Developed by Krishna Gorai\n");
+    }
+
+    return 0;
+}
