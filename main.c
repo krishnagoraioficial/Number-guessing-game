4e208c832e8176dc569b0281c5f928d326dc69aa
krishnagoraioficial <krishnagoraioficial@gmail.com>
2025-04-17 18:11:38 +0530

    Add the final touch and fixed the bugs

--- main.c
+++ main.c
@@ -6,44 +6,51 @@
 int main()
 {
     // Variables
-    char Play = 'Y';
+    char Play;
     int guess;
     int random;
-    int count = 1;
-    // Generating a random numbers using rand function
+    int count;
+
     srand(time(0));
-    random = (rand() % 100) + 1;
 
-    // ####### Welcome Window ######## //
-
+    // <<<<<<<<<<<<<<<<Start the game>>>>>>>>>>>>>
     printf("\t====== Welcome to Number gussing game ======\n\n");
 
     
 do{
-    here: // Lebel for goto function
+    random = (rand() % 100) + 1;
+    count = 1;
+
     printf("Enter a number between 1 to 100: ");
-    scanf("%d", &guess);
-    if (guess < random)
-    {
-        printf("Try a big number: ");
-        count++;
-        goto here;
+
+    while (1) {
+        if (scanf("%d", &guess) != 1) {
+            // Check invalid input (non-integer)
+            printf("Invalid input! Please enter a number: ");
+            while (getchar() != '\n'); // Clear input buffer
+            continue;
+        }
+
+        if (guess < random) {
+            printf("Try a bigger number: ");
+            count++;
+        } else if (guess > random) {
+            printf("Try a smaller number: ");
+            count++;
+        } else {
+            printf("\n\t+++ Congrats you got the number in %d tries +++\n", count);
+            break;  
+            //<<<<<<<<<<<<<End the game>>>>>>>>>>>>>>
+        }
     }
-    else if (guess > random)
-    {
-        printf("Try small number: ");
-        count++;
-        goto here;
-    }
-    else
-    {
-        printf("\n\t+++ Congrats you got the number in %d try +++\n", count);
-        printf("Do you want to play more[y/n]? ");
-        scanf("%c", &Play);
-        while (getchar() != '\n');  // Protect buffer overflow
-        Play = toupper(Play);
-    }
-}while(Play == 'Y');
+
+    // Reapet the game //
+    printf("Do you want to play more [y/n]? ");
+    while (getchar() != '\n');  // Clear input buffer 
+    scanf("%c", &Play);
+    Play = toupper(Play);
+
+} while (Play == 'Y');
 
     printf("\nThanks for Playing\n@ Developed by Krishna Gorai\n");
     return 0;
